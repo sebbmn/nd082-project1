e701a4822d6a0134a0135a1cee884e72667f2f16
sebbmn <sebbmn@users.noreply.github.com>
2021-08-13 11:16:13 +0200

    feat: az policy definition & assignment create

--- commands.bash
+++ commands.bash
@@ -1,5 +1,8 @@
+#### Policies ####
 # Create the Policy Definition (Subscription scope)
-az policy definition create --name tagging-policy --display-name "Deny untagged resources deployement" --description "This policy check if a tag is present on a resource and deny deployement otherwise" --params policy.parameters.json --rules policy.rules.json --mode All
+az policy definition create --name tagging-policy --display-name "Deny untagged resources deployement" --description "This policy check if a tag is present on a resource and deny deployement otherwise" --rules policy.rules.json --mode All
 
 # Create the Policy Assignment
 az policy assignment create --name 'tagging-policy-assignment' --display-name "Deny untagged resources deployement Assignment" --scope /subscriptions/<subscriptionId> --policy /subscriptions/<subscriptionId>/providers/Microsoft.Authorization/policyDefinitions/tagging-policy
+
+#### ... ####
