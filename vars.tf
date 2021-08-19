7caabbf3f3f8d2881b83b5095367124cac5f74af
sebbmn <sebbmn@users.noreply.github.com>
2021-08-19 16:34:07 +0200

    feat: terraform template 1st draft

---
+++ vars.tf
@@ -0,0 +1,17 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this project"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this project should be created."
+}
+
+variable "instance_count" {
+  type        = number
+  description = "Number of VM instances to be created"
+
+  validation {
+    condition     = var.instance_count > 2 && var.instance_count <= 5
+    error_message = "Number of VM instances MUST be between 2 and 5"
+  }
+}
